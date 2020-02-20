bc1cd0cc42d55c6a85b858428def872f3dc1e758
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-20 04:52:22 +0000

    20th feb

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,37 +3,104 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'src/<%= pkg.name %>.js',
-    //     dest: 'build/<%= pkg.name %>.min.js'
-    //   }
-    // }
-      sass: {                              // Task
-        dist: {                            // Target
-          options: {                       // Target options
-            style: 'expanded'
+    uglify: {
+
+      build: {
+        src: 'js/script.js',
+        dest: 'js/script.min.js'
+      }
+    },
+    watch: {
+      all: {
+        files: ['sass/style.scss', 'css/style.css', 'js/script.js'],
+        tasks: ['sass','csslint','jshint']
+      }
+    },
+    sass: {                              // Task
+    dist: {                            // Target
+      options: {                       // Target options
+      style: 'expanded'
       },
       files: {                         // Dictionary of files
-        'css/style.css': 'sass/style.scss',       // 'destination': 'source'
+        'css/style.css': 'sass/style.scss'    // 'destination': 'source'
+
       }
       }
     },
+    csslint: {
+      // strict: {
+      //   options: {
+      //     import: 2
+      //   },
+      //   src: ['css/*.css','!*.min.css']//do not include min files
+      // },
+      lax: {
+        options: {
+          import: false,
+          'order-alphabetical' :false
+        },
+        src: ['css/*.css','!*.min.css']
+      }
+    },
     jshint: {
-    all: ['Gruntfile.js', 'js/*.js']
-  }
+      all: ['Gruntfile.js', 'js/*.js']
+    },
+    imagemin: {
+       static: {
+        options: {
+               optimizationLevel: 3,
+               svgoPlugins: [{removeViewBox: false}],
+               // use: [mozjpeg()] // Example plugin usage
+           },
+           files: {
+               'dist/img.png': 'src/img.png',
+               'dist/img.jpg': 'src/img.jpg',
+               'dist/img.gif': 'src/img.gif'
+           }
+       },
+       dynamic: {
+           files: [{
+               expand: true,
+               cwd: 'src/',
+               src: ['**/*.{png,jpg,gif}'],
+               dest: 'dist/'
+           }]
+       }
+   },
+   htmlmin: {                                     // Task
+    dist: {                                      // Target
+      options: {                                 // Target options
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      files: {                                   // Dictionary of files
+        'dist/index.html': 'src/index.html',     // 'destination': 'source'
+        'dist/contact.html': 'src/contact.html'
+      }
+    },
+    dev: {                                       // Another target
+      files: {
+        'dist/index.html': 'src/index.html',
+        'dist/contact.html': 'src/contact.html'
+      }
+    }
+  },
   });
 
   // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-      grunt.loadNpmTasks('grunt-contrib-sass');
-      grunt.loadNpmTasks('grunt-contrib-jshint');
-  // // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
-      grunt.registerTask('test', ['sass','jshint']);
+
+
+  // Default task(s).
+    grunt.registerTask('default', ['htmlmin']);
+    grunt.registerTask('ugly', ['uglify']);
+    grunt.registerTask('default', ['watch']); //run this using grunt test
 
 };
--- js/script.js
+++ js/script.js
@@ -2,6 +2,49 @@ console.log('NPM-GRUNT');
 
 $(document).ready(function(){
   $('body').click(function(){
-    $(this).css('background','cyan');
+    $(this).css('background','red');
   });
 });
+
+$(document).ready(function(){
+	// accessing key from json file
+	var myKey = JSON.parse(apiKey);
+	console.log(myKey[0]);
+	myKey = myKey[0].key;
+	console.log(myKey);
+
+//
+	$.ajax({
+	  url : `http://newsapi.org/v2/top-headlines?country=us&apiKey=${myKey}`,
+		type :'GET',
+		data :'json',
+		success:function(data){
+			console.log(data);
+
+        var o = data.articles[0].title;
+        var a = data.articles[0].author;
+        console.log(o);
+        console.log(a);
+
+      var i;
+      // for (i = 0; i < data.articles.length; i++) {
+      //
+      // document.getElementById('result').innerHTML +=
+      // '<div class="col col-sm-6 col-md-3 col-lg-3">' +
+      // '<img class="img-thumbnal w-50" src="' + data.photos[i].img_src + '"></img>' +
+      // '<p>ID: ' + data.photos[i].id + '</p>' +
+      // '<p>Date: ' + data.photos[i].earth_date + '</p>' +
+      // '</div>'
+      // for (i = 0; i < data.features.length; i++) {
+      //   for (j = 0; j < data.features[i].geometry.coordinates.length; j++) {
+      //     console.log(data.features[i].geometry.coordinates);
+        // }
+      // }
+      // }
+    },
+    error:function(){
+		console.log('error');
+		}
+
+	});//ajax
+}); //document ready
