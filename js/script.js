f29c537979493db7f950dfa50687c751c1ae2597
kristine <brooks09@gmail.com>
2020-02-23 22:13:46 +1300

    added javascrip to filter and created a search function

--- js/script.js
+++ js/script.js
@@ -1,59 +1,62 @@
-console.log('NPM-GRUNT');
+$(document).ready(function() {
 
-// $(document).ready(function(){
-//   $('body').click(function(){
-//     $(this).css('background','yellow');
-//   });
-// });
+//here the variable is taking the api key from the JSON file in order to use it later to get data.
+  var myKey = JSON.parse(apiKey);
+  myKey = myKey[0].key;
 
-$(document).ready(function(){
-	// accessing key from json file
-	var myKey = JSON.parse(apiKey);
-	console.log(myKey[0]);
-	myKey = myKey[0].key;
-	console.log(myKey);
 
-// filter choices
-document.getElementBYid('submit').addEventListener('click', function(){
-    country = document.getElementByid('endpoints').value;
-        size = documemt.getElementBYid('size').value;
 
-        console.log(endPoint,size);
-        var
+  // #submit is the ID taken from index.html file for the submit  button 
+  $('#submit').on('click', function(e) {
+    // created a variable for country and catergory the  
+    var country = $('#country option:selected').val();
+    var category = $('#category option:selected').val();
+    var searchTerm = $('#search-term').val();
+    showNewsArticles(country, category, searchTerm);
+  });
+
+  function showNewsArticles(country,category, searchTerm) {
+    // Url from NewsAPI.org - specifically targeted country and category to get the specific data
+    var url;
+    if(searchTerm !== '') {
+      url = `http://newsapi.org/v2/top-headlines?q=${searchTerm}&country=${country}&category=${category}&apiKey=${myKey}`;
+    } else {
+      url = `http://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${myKey}`;
+    }
+
+    console.log('The url is %s', url);
+    
+
+    // ajax method is used to call the requested data from the newaAPI URL. 
+    $.ajax({
+      url : url,
+
+      type :'GET',
+      data :'json',
+      success:function(data){
+        $('#api-results').empty();
+
+        // here we are targeting the data and pulling from it are the articlesrelated to the chosen results from the country and catergory chosen.. 
+        if(data.articles.length > 0) {
+          $('#api-results').html(`<div class="col my-5">`);
+
+          // styled data on bootstrap card 
+          for(var i = 0; i < data.articles.length; i++) {
+            $('#api-results').append(
+              '<div class="card">'+
+                `<img src="` + data.articles[i].urlToImage + `" class="card-img-top" alt="...">`+
+                '<div class="card-body">'+
+                  `<h5 class="card-title">` + data.articles[i].title + `</h5>`+
+                  `<p class="card-text">` + data.articles[i].description + `</p>`+
+                '</div>'+
+              '</div>'
+            );
+          }
+          $('#api-results').append('</div>');
+        } else {
+          $('#api-results').html('There are no results.');
+        }
+      }
+    }); 
+  }
 });
-
-//
-	$.ajax({
-	  url : `http://newsapi.org/v2/top-headlines?country=us&apiKey=${myKey}`,
-		type :'GET',
-		data :'json',
-		success:function(data){
-			console.log(data);
-
-        var o = data.articles[0].title;
-        var a = data.articles[0].author;
-        console.log(o);
-        console.log(a);
-
-      var i;
-      // for (i = 0; i < data.articles.length; i++) {
-      //
-      // document.getElementById('result').innerHTML +=
-      // '<div class="col col-sm-6 col-md-3 col-lg-3">' +
-      // '<img class="img-thumbnal w-50" src="' + data.photos[i].img_src + '"></img>' +
-      // '<p>ID: ' + data.photos[i].id + '</p>' +
-      // '<p>Date: ' + data.photos[i].earth_date + '</p>' +
-      // '</div>'
-      // for (i = 0; i < data.features.length; i++) {
-      //   for (j = 0; j < data.features[i].geometry.coordinates.length; j++) {
-      //     console.log(data.features[i].geometry.coordinates);
-        // }
-      // }
-      // }
-    },
-    error:function(){
-		console.log('error');
-		}
-
-	});//ajax
-}); //document ready
